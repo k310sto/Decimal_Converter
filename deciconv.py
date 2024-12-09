5e469925cde6af26dd280396dfad565c866016f0
k310sto <k310.sto@gmail.com>
2024-12-09 07:59:56 +0000

    3rd com

--- deciconv.py
+++ deciconv.py
@@ -8,15 +8,18 @@ import sys
 if(len(sys.argv)==1):
     n = 2
 if(len(sys.argv)==2):
-    if not(str(sys.argv[1]).isdigit()):
+    if not(sys.argv[1].isdigit()):
         sys.exit("Error: Invalid argument")
     else:
-        if(int(sys.argv[1])<0 and 100<int(sys.argv[1])):
+        if(int(sys.argv[1])<0 or 62<int(sys.argv[1])):
             sys.exit("Error: Argument out of range")
         else:
             n = int(sys.argv[1])
 
+
 stdin = sys.stdin.readline()
+if not (stdin.strip().isdigit()):
+    sys.exit("Error: Invalid input")
 rem = []
 quo = int(stdin)
 i = 0
@@ -24,10 +27,13 @@ while quo != 0:
     r = quo % n
     if r < 10:
         rem.insert(i, str(r))
+    elif r > 35:
+        rem.insert(i, chr(r+61))
     else:
         rem.insert(i, chr(r+55))
     quo = quo // n
     i += 1
 rem.reverse()
 ans = ''.join(map(str, rem))
+
 print(ans)
