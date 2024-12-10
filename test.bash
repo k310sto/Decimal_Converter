95a7c4738aa022ff3d81bc53ec2352e9a3f032d2
k310sto <k310.sto@gmail.com>
2024-12-10 04:10:29 +0000

    5th com

---
+++ test.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Kensuke Sato
+# SPDX-License-Identifier: BSD-3-Clause
+
+res=0
+sh install_decicon.sh
+res=$?
+[ "${res}" = 1 ] && echo "install error" && exit ${res}
+
+echo 840 | decicon 50
+res=$?
+[ "${res}" = Ge ] && echo "command error" && exit ${res}
+
+bash uninstall_bash.sh
+res=$?
+[ "${res}" = 1 ] && echo "uninstall error"
+
+[ "${res}" = 0 ] && echo "all clear"
+exit ${res}
