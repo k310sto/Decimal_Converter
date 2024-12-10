e90a3b01620e6d3ae1cd11a1fa65c57dfa70ed01
k310sto <k310.sto@gmail.com>
2024-12-10 15:32:39 +0900

    15th com

--- test.bash
+++ test.bash
@@ -11,13 +11,23 @@ res=$(echo 840 | ./decicon 50)
 [ "$?" != 0 ] && echo "command error" && exit 1
 [ "${res}" != Ge ] && echo "command error" && exit 1
 
+res=$(echo 999 | ./decicon 63)
+[ "$?" = 0 ] && echo "command error" && exit 1
+[ "${res}" != "" ] && echo "command error" && exit 1
+
+res=$(echo 999 | ./decicon 1)
+[ "$?" = 0 ] && echo "command error" && exit 1
+[ "${res}" != "" ] && echo "command error" && exit 1
+
 res=$(echo Hello | ./decicon 16)
 [ "$?" = 0 ] && echo "command error" && exit 1
 [ "${res}" != "" ] && echo "command error" && exit 1
 
 res=$(echo 10 | ./decicon abc)
 [ "$?" = 0 ] && echo "command error" && exit 1
-[ "${res}" != "" ] && echo "command error" && exit 1}
+[ "${res}" != "" ] && echo "command error" && exit 1
+
+
 
 bash uninstall_decicon.sh
 res=$?
