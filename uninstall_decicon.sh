14fce07d533ceeeb2fcb4fb00b23de8f501f7111
k310sto <k310.sto@gmail.com>
2024-12-11 16:47:14 +0900

    Delete uninstall_decicon.sh

--- uninstall_decicon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: 2024 Kensuke Sato
-# SPDX-License-Identifier: BSD-3-Clause
-
-cd ~
-if grep -q "export PATH+=:~/Decimal_Converter/" "./.bashrc"; then
-	sed -i -e "s@export PATH+=:~/Decimal_Converter/@@g" ~/.bashrc;
-fi
