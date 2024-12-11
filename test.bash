882e37902c1694568dec7e77432bafa6ba574be2
k310sto <k310.sto@gmail.com>
2024-12-11 16:46:33 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -3,10 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 res=0
-sh install_decicon.sh
-res=$?
-[ "${res}" = 1 ] && echo "install error" && exit 1
-
 res=$(echo 840 | ./decicon 50)
 [ "$?" != 0 ] && echo "command error" && exit 1
 [ "${res}" != Ge ] && echo "command error" && exit 1
@@ -26,12 +22,6 @@ res=$(echo Hello | ./decicon 16)
 res=$(echo 10 | ./decicon abc)
 [ "$?" = 0 ] && echo "command error" && exit 1
 [ "${res}" != "" ] && echo "command error" && exit 1
-
-
-
-bash uninstall_decicon.sh
-res=$?
-[ "${res}" = 1 ] && echo "uninstall error"
 
 [ "${res}" = 0 ] && echo "all clear"
 exit ${res}
